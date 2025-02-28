883a5d16fff6b619a5c549c504e3b11c7465af87
ksyk1205 <ksyk1205@naver.com>
2025-02-28 18:57:51 +0900

    programmers - targetNumber

--- src/codingtest/programmers/TargetNumber.java
+++ src/codingtest/programmers/TargetNumber.java
@@ -1,12 +1,22 @@
 package codingtest.programmers;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TargetNumber {
 
   public static void main(String[] args) {
     int[] numbers = {1, 1, 1, 1, 1};
     int target = 3;
 
-    System.out.println(solution(numbers, target));
+//    System.out.println(solution(numbers, target)); //5
+    System.out.println(bfs(numbers, target)); //5
+
+    int[] numbers2 = {4, 1, 2, 1};
+    int target2 = 4;
+
+//    System.out.println(solution(numbers2, target2)); // 2
+    System.out.println(bfs(numbers2, target2)); // 2
   }
 
   private static int solution(int[] numbers, int target) {
@@ -24,5 +34,38 @@ public class TargetNumber {
     return plus + minus;
   }
 
+
+  private static int bfs(int[] numbers, int target) {
+    int n = numbers.length;
+
+    Queue<Integer> sumQueue = new LinkedList<>();
+    Queue<Integer> indexQueue = new LinkedList<>();
+
+    sumQueue.add(numbers[0]);
+    sumQueue.add(-1 * numbers[0]);
+    indexQueue.add(0);
+    indexQueue.add(0);
+
+    int count = 0;
+
+    while (!sumQueue.isEmpty()) {
+      Integer sum = sumQueue.poll();
+      Integer index = indexQueue.poll();
+
+      if (index == n-1) {
+        if (sum == target) {
+          count++;
+        }
+      } else {
+        sumQueue.add(sum + numbers[index+1]);
+        indexQueue.add(index+1);
+
+        sumQueue.add(sum + (-1 * numbers[index+1]));
+        indexQueue.add(index+1);
+      }
+    }
+
+    return count;
+  }
 
 }
